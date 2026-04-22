632c6b4167938e804230a9fc9f69a61e0b64ce9f
Zachary Snow <zachary.snow@gmail.com>
2026-04-22 21:08:25 +0200

    Update 2026-04: Update to latest versions of go and sdl

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/smack0007/sdl3-go
 
-go 1.26.1
+go 1.26.2
--- tools/sdlinfo/main.go
+++ tools/sdlinfo/main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 
+	IMG "github.com/smack0007/sdl3-go/img"
 	SDL "github.com/smack0007/sdl3-go/sdl"
 )
 
 func main() {
 	fmt.Printf("=== Compiled ===\n")
-	fmt.Printf("Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(SDL.VERSION), SDL.VERSIONNUM_MINOR(SDL.VERSION), SDL.VERSIONNUM_MICRO(SDL.VERSION))
-	fmt.Printf("Revision: %s\n", SDL.REVISION)
+	fmt.Printf("SDL Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(SDL.VERSION), SDL.VERSIONNUM_MINOR(SDL.VERSION), SDL.VERSIONNUM_MICRO(SDL.VERSION))
+	fmt.Printf("SDL Revision: %s\n", SDL.REVISION)
+	fmt.Printf("SDL_image Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(IMG.VERSION), SDL.VERSIONNUM_MINOR(IMG.VERSION), SDL.VERSIONNUM_MICRO(IMG.VERSION))
 
 	fmt.Printf("\n")
 
 	fmt.Printf("=== Linked ===\n")
-	version := SDL.GetVersion()
-	fmt.Printf("Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(version), SDL.VERSIONNUM_MINOR(version), SDL.VERSIONNUM_MICRO(version))
-	fmt.Printf("Revision: %s\n", SDL.GetRevision())
+	sdlVersion := SDL.GetVersion()
+	fmt.Printf("SDL Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(sdlVersion), SDL.VERSIONNUM_MINOR(sdlVersion), SDL.VERSIONNUM_MICRO(sdlVersion))
+	fmt.Printf("SDL Revision: %s\n", SDL.GetRevision())
+	imgVersion := IMG.GetVersion()
+	fmt.Printf("SDL_image Version: %d.%d.%d\n", SDL.VERSIONNUM_MAJOR(imgVersion), SDL.VERSIONNUM_MINOR(imgVersion), SDL.VERSIONNUM_MICRO(imgVersion))
 }
